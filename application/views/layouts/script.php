b1628a9d01763d52c92c270cd7f3e507fec7714c
rgibranz <rgibran234@gmail.com>
2021-09-14 17:59:10 +0700

    ganti tampilan login

--- application/views/layouts/script.php
+++ application/views/layouts/script.php
@@ -1,12 +1,1 @@
-<!--   Core JS Files   -->
-<script src="<?= base_url() ?>assets/js/core/jquery.min.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/plugins/moment.min.js"></script>
-<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-<script src="<?= base_url() ?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="<?= base_url() ?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-<!--  Google Maps Plugin    -->
-<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-<script src="<?= base_url() ?>assets/js/material-kit.js?v=2.0.7" type="text/javascript"></script>
+<script src="<?= site_url('assets/js/mdb.min.js') ?>"></script>
--- application/views/login/user.php
+++ application/views/login/user.php
@@ -5,49 +5,61 @@
     <?php $this->load->view('layouts/header.php') ?>
 </head>
 
-<body class="login-page sidebar-collapse">
-    <div class="page-header header-filter" style="background-image: url('<?= base_url() ?>assets/img/bg7.jpg'); background-size: cover; background-position: top center;">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 ml-auto mr-auto">
-                    <div class="card card-login">
-                        <form class="form" method="post" action="<?= site_url('auth') ?>" style='min-height: 320px;'>
-                            <div class="card-header card-header-primary text-center">
-                                <h4 class="card-title">Login Sebelum Melanjutkan</h4>
-                            </div>
-                            <div class="card-body">
+<body>
+    <!--Main Navigation-->
+    <header>
+        <style>
+            #intro {
+                background-image: url(https://mdbootstrap.com/img/new/fluid/city/008.jpg);
+                height: 100vh;
+            }
 
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">mail</i>
-                                        </span>
-                                    </div>
-                                    <input type="email" class="form-control" placeholder="Email..." name="email" </div>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">
-                                                <i class="material-icons">lock_outline</i>
-                                            </span>
-                                        </div>
-                                        <input type="password" class="form-control" placeholder="Password..." name="password">
-                                    </div>
+            /* Height for devices larger than 576px */
+            @media (min-width: 992px) {
+                #intro {
+                    margin-top: -58.59px;
+                }
+            }
+        </style>
+
+        <!-- Background image -->
+        <div id="intro" class="bg-image shadow-2-strong">
+            <div class="mask d-flex align-items-center h-100">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-5 col-md-8">
+                            <form class="bg-white  rounded-5 shadow-5-strong p-5" method="post" action="<?= site_url('auth') ?>">
+
+                                <h4 class="mb-1">Login</h4>
+                                <!-- Email input -->
+                                <div class="form-outline mb-4">
+                                    <input type="email" id="form1Example1" class="form-control" name="email" />
+                                    <label class="form-label" for="form1Example1">Email address</label>
                                 </div>
+
+                                <!-- Password input -->
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="form1Example2" class="form-control" name="password" />
+                                    <label class="form-label" for="form1Example2">Password</label>
+                                </div>
+
                                 <?php if ($this->session->flashdata('auth')) : ?>
                                     <div class="alert alert-danger" role="alert">
                                         <?= $this->session->flashdata('auth') ?>
                                     </div>
                                 <?php endif ?>
-                                <div class="footer text-center">
-                                    <button type="submit" class="btn btn-primary btn-primary btn-wd btn-lg">Login</button>
-                                </div>
-                        </form>
+
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
+        <!-- Background image -->
+    </header>
+    <!--Main Navigation-->
 </body>
 
 </html>
