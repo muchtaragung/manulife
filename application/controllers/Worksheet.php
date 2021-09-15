5714446a379aa16381590c098fb6d0328d49b362
rgibranz <rgibran234@gmail.com>
2021-09-15 13:26:16 +0700

    menyimpan goal

--- application/controllers/Worksheet.php
+++ application/controllers/Worksheet.php
@@ -15,7 +15,7 @@ class Worksheet extends CI_Controller
      *
      * @return void
      */
-    public function competence_dan_motivation()
+    public function worksheet()
     {
         $data['page_title'] = "Worksheet";
 
@@ -27,18 +27,59 @@ class Worksheet extends CI_Controller
      *
      * @return void
      */
-    public function save_competence_dan_motivation()
+    public function save_worksheet()
     {
+        // mengambil lalu menyimpan data goal
+        $goal['goal'] = $this->input->post('goal');
+        $this->save_goal($goal);
+        $goal_id = $this->db->insert_id();
+
         // mengambil data untuk competence
         $competence['knowledge'] = $this->input->post('knowledge');
         $competence['skill']     = $this->input->post('skill');
+        $competence['goal_id']   = $goal_id;
+        $this->save_competence($competence);
 
         // mengambil data untuk motivation
         $motivation['comitment']  = $this->input->post('comitment');
         $motivation['confidence'] = $this->input->post('confidence');
+        $motivation['goal_id']   = $goal_id;
+        $this->save_motivation($competence);
 
-        // menyimpan data
+        $this->get_level($competence, $motivation);
+        $this->get_style($competence, $motivation);
+    }
+
+    /**
+     * menyimpan goal
+     *
+     * @param array $goal
+     * @return void
+     */
+    private function save_goal(array $goal)
+    {
+        $this->Goal_model->save($goal);
+    }
+
+    /**
+     * menyimpan competence
+     *
+     * @param array $competence
+     * @return void
+     */
+    private function save_competence(array $competence)
+    {
         $this->Competence_model->save($competence);
+    }
+
+    /**
+     * menyimpan motivation
+     *
+     * @param array $motivation
+     * @return void
+     */
+    private function save_motivation(array $motivation)
+    {
         $this->Motivation_model->save($motivation);
     }
 }
