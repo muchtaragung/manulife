fee7779d908ffa060312cbdb89cfe5ff7a08f37f
rgibranz <rgibran234@gmail.com>
2021-09-16 09:58:06 +0700

    tambah card di behaviour dan worksheet

--- application/views/worksheet.php
+++ application/views/worksheet.php
@@ -10,74 +10,78 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-sm-12 mx-auto">
-                <form action="<?= site_url('worksheet/save_worksheet') ?>" method="POST">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <label for="">Goals</label>
-                            <br>
-                            <textarea name="goal" id="" cols="30" rows="10" class="form-control"></textarea>
-                        </div>
-                        <div class="col-md-6 col-sm-12 justify-content-center">
-                            <h3>Competence</h3>
-                            <div class="form-group">
-                                <label for="">Skill</label>
-                                <br>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name='skill' type="radio" value="low">
-                                    <label class="form-check-label">Low</label>
+                <div class="card my-5 text-center">
+                    <form action="<?= site_url('worksheet/save_worksheet') ?>" method="POST">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label for="">Goals</label>
+                                    <br>
+                                    <textarea name="goal" id="" cols="30" rows="10" class="form-control"></textarea>
                                 </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name='skill' type="radio" value="high">
-                                    <label class="form-check-label">High</label>
+                                <div class="col-md-6 col-sm-12 justify-content-center">
+                                    <h3>Competence</h3>
+                                    <div class="form-group">
+                                        <label for="">Skill</label>
+                                        <br>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name='skill' type="radio" value="low">
+                                            <label class="form-check-label">Low</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name='skill' type="radio" value="high">
+                                            <label class="form-check-label">High</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Knowledge</label>
+                                        <br>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name='knowledge' type="radio" value="low">
+                                            <label class="form-check-label">Low</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name='knowledge' type="radio" value="high">
+                                            <label class="form-check-label">High</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12 justify-content-center">
+                                    <h3>Motivation</h3>
+                                    <div class="form-group">
+                                        <label for="">Comitment</label>
+                                        <br>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name="comitment" type="radio" value="low">
+                                            <label class="form-check-label">Low</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name="comitment" type="radio" value="high">
+                                            <label class="form-check-label">High</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Confidence</label>
+                                        <br>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name="confidence" type="radio" value="low">
+                                            <label class="form-check-label">Low</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" name="confidence" type="radio" value="high">
+                                            <label class="form-check-label">High</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <button type="submit" class="btn btn-lg btn-primary">
+                                        Submit
+                                    </button>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="">Knowledge</label>
-                                <br>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name='knowledge' type="radio" value="low">
-                                    <label class="form-check-label">Low</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name='knowledge' type="radio" value="high">
-                                    <label class="form-check-label">High</label>
-                                </div>
-                            </div>
                         </div>
-                        <div class="col-md-6 col-sm-12 justify-content-center">
-                            <h3>Motivation</h3>
-                            <div class="form-group">
-                                <label for="">Comitment</label>
-                                <br>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name="comitment" type="radio" value="low">
-                                    <label class="form-check-label">Low</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name="comitment" type="radio" value="high">
-                                    <label class="form-check-label">High</label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Confidence</label>
-                                <br>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name="confidence" type="radio" value="low">
-                                    <label class="form-check-label">Low</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" name="confidence" type="radio" value="high">
-                                    <label class="form-check-label">High</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <button type="submit" class="btn btn-lg btn-primary">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
