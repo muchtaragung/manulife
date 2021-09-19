6621be99ddf60ba677555491e51f629875cb643d
rgibranz <rgibran234@gmail.com>
2021-09-19 20:08:33 +0700

    Revert "menghapus level dan style di database dan juga modelnya"
    This reverts commit 2eebbf8230fcef4e3190523849309e4496223dec.

--- application/controllers/Worksheet.php
+++ application/controllers/Worksheet.php
@@ -11,6 +11,8 @@ class Worksheet extends CI_Controller
         $this->load->model('Goal_model');
         $this->load->model('Competence_model');
         $this->load->model('Motivation_model');
+        $this->load->model('Level_model');
+        $this->load->model('Style_model');
         $this->load->model('Supporting_model');
         $this->load->model('Directing_model');
         $this->load->model('Peserta_model');
---
+++ application/models/Style_model.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Style_model extends CI_Model
+{
+    private $table = 'leadership_style';
+    /**
+     * menyimpan data ke tabel
+     *
+     * @param array $object
+     * @return void
+     */
+    public function save(array $object)
+    {
+        $this->db->insert($this->table, $object);
+    }
+
+    /**
+     * mengambil data tabel dengan kondisi where
+     *
+     * @param array $where array dari data yang mau di ambil
+     * @return void
+     */
+    public function get_where(array $where)
+    {
+        return $this->db->get_where($this->table, $where);
+    }
+}
