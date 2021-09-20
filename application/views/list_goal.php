691b6a81addcdca5381e1f08cdc8169664abd333
rgibranz <rgibran234@gmail.com>
2021-09-20 15:31:45 +0700

    fix tulisan di list goall

--- application/views/list_goal.php
+++ application/views/list_goal.php
@@ -19,8 +19,8 @@
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Tabel Peserta</h3>
-                            <p class="text-subtitle text-muted">List Dari Peserta</p>
+                            <h3>Tabel Goal</h3>
+                            <p class="text-subtitle text-muted">List Goal Dari Peserta</p>
 
                         </div>
                     </div>
