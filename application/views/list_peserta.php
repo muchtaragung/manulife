821c412ce6331dd6596b7fc8647b1221661de13e
rgibranz <rgibran234@gmail.com>
2021-09-20 11:06:41 +0700

    tampilan list peserta

--- application/views/list_peserta.php
+++ application/views/list_peserta.php
@@ -2,86 +2,78 @@
 <html lang="en">
 
 <head>
-    <?php $this->load->view('layouts/header.php') ?>
+    <?php $this->load->view('layouts/header') ?>
 </head>
 
 <body>
+    <div id="app">
+        <?php $this->load->view('layouts/sidebar') ?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-sm-12 mx-auto">
-                <div class="card my-5 shadow-5-strong">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-12 mx-1">
-                                <!-- Button trigger modal -->
-                                <button class="btn btn-primary float-right" data-mdb-toggle="modal" data-mdb-target="#exampleModal">
-                                    Tambah Peserta
-                                </button>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <form action="<?= site_url('peserta/save') ?>" method="post">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Tambah Peserta</h5>
-                                                    <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="form-group">
-                                                        <label for="">Nama</label>
-                                                        <input type="text" name="nama" id="" class="form-control">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="">Email</label>
-                                                        <input type="email" name="email" id="" class="form-control">
-                                                    </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
-                                                        Close
-                                                    </button>
-                                                    <button type="submit" class="btn btn-success">Save</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                                <table class="table text-center">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Nama</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col">Act</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php $a = 1;
-                                        foreach ($peserta as $pes) : ?>
-                                            <tr>
-                                                <td><?= $a++ ?></td>
-                                                <td><?= $pes->nama ?></td>
-                                                <td><?= $pes->email ?></td>
-                                                <td>
-                                                    <a href="<?= site_url('goals/' . $pes->id) ?>" class="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">
-                                                        Lihat Goal
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        <?php endforeach ?>
-                                    </tbody>
-                                </table>
-                            </div>
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Tabel Peserta</h3>
+                            <p class="text-subtitle text-muted">List Dari Peserta</p>
                         </div>
                     </div>
                 </div>
+                <section class="section">
+                    <div class="card">
+                        <div class="card-header">
+                            Tabel Peserta
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped" id="table1">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nama</th>
+                                        <th>Email</th>
+                                        <th>Act</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $a = 1;
+                                    foreach ($peserta as $pes) : ?>
+                                        <tr>
+                                            <td><?= $a++ ?></td>
+                                            <td><?= $pes->nama ?></td>
+                                            <td><?= $pes->email ?></td>
+                                            <td>
+                                                <a href="<?= site_url('goals/' . $pes->id) ?>" class="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">
+                                                    Lihat Goal
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </section>
             </div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Korpora Consulting</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="http://korporaconsulting.com">Korpora Consulting</a></p>
+                    </div>
+                </div>
+            </footer>
         </div>
     </div>
-
-    <?php $this->load->view('layouts/script.php') ?>
+    <?php $this->load->view('layouts/script') ?>
 </body>
 
 </html>
