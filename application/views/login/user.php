1b054794663975ea3a057e095c2938c1d5af4f83
rgibranz <rgibran234@gmail.com>
2021-09-20 10:49:48 +0700

    tampilan login mazer

--- application/views/login/user.php
+++ application/views/login/user.php
@@ -2,64 +2,68 @@
 <html lang="en">
 
 <head>
-    <?php $this->load->view('layouts/header.php') ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('') ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url('') ?>assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?= base_url('') ?>assets/css/app.css">
+    <link rel="stylesheet" href="<?= base_url('') ?>assets/css/pages/auth.css">
 </head>
 
 <body>
-    <!--Main Navigation-->
-    <header>
-        <style>
-            #intro {
-                background-image: url(https://mdbootstrap.com/img/new/fluid/city/008.jpg);
-                height: 100vh;
-            }
+    <div id="auth">
 
-            /* Height for devices larger than 576px */
-            @media (min-width: 992px) {
-                #intro {
-                    margin-top: -58.59px;
-                }
-            }
-        </style>
-
-        <!-- Background image -->
-        <div id="intro" class="bg-image shadow-2-strong">
-            <div class="mask d-flex align-items-center h-100">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-5 col-md-8">
-                            <form class="bg-white  rounded-5 shadow-5-strong p-5" method="post" action="<?= site_url('auth') ?>">
-
-                                <h4 class="mb-1">Login</h4>
-                                <!-- Email input -->
-                                <div class="form-group mb-4">
-                                    <label for="form1Example1">Email address</label>
-                                    <input type="email" id="form1Example1" class="form-control" name="email" />
-                                </div>
-
-                                <!-- Password input -->
-                                <div class="form-group mb-4">
-                                    <label for="form1Example2">Password</label>
-                                    <input type="password" id="form1Example2" class="form-control" name="password" />
-                                </div>
-
-                                <?php if ($this->session->flashdata('auth')) : ?>
-                                    <div class="alert alert-danger" role="alert">
-                                        <?= $this->session->flashdata('auth') ?>
-                                    </div>
-                                <?php endif ?>
-
-                                <!-- Submit button -->
-                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                            </form>
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-5">Silahkan Login Untuk Melanjutkan</p>
+                    <?php if ($this->session->flashdata('auth')) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $this->session->flashdata('auth') ?>
                         </div>
+                    <?php endif ?>
+                    <form method="post" action="<?= site_url('auth') ?>">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl" placeholder="Email" name="email">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <!-- <div class="form-check form-check-lg d-flex align-items-end">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                Keep me logged in
+                            </label>
+                        </div> -->
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <!-- <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
+                                up</a>.</p>
+                        <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p> -->
                     </div>
                 </div>
             </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
         </div>
-        <!-- Background image -->
-    </header>
-    <!--Main Navigation-->
+
+    </div>
 </body>
 
 </html>
