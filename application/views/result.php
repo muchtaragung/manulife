9cd4550fd302720338bbdcba84f79452b47e9ae5
rgibranz <rgibran234@gmail.com>
2021-10-06 15:09:46 +0700

    hapus page title result

--- application/views/result.php
+++ application/views/result.php
@@ -16,7 +16,7 @@
             </header>
 
             <div class="page-heading">
-                <div class="page-title">
+                <!-- <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
                             <h3>Result</h3>
@@ -24,9 +24,9 @@
 
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <section class="section">
-                    <div class="card my-5 text-center shadow-5-strong">
+                    <div class="card text-center shadow-5-strong">
                         <div class="card-body">
                             <h1 class="card-title">Result</h1>
                             <br>
